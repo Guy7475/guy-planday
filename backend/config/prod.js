8e26fc4418cb8de7f6213059c9ca90ae4a818524
Guy <orcabasic@gmail.com>
2022-06-04 13:08:41 +0300

    env

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-  // dbURL: 'mongodb+srv://test:1234@cluster1.ellxz.mongodb.net/?retryWrites=true&w=majority',
   dbURL: `mongodb+srv://${process.env.DB_KEY}.ellxz.mongodb.net/?retryWrites=true&w=majority`,
 }
